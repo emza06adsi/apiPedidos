1fc418c517c2a74d96787c5a984744e398d88c42
esteban <emeza06@misena.edu.co>
2020-02-25 16:58:41 -0500

    base de datos conectada correcta mentee

--- sotore/gadolDbMysql.js
+++ sotore/gadolDbMysql.js
@@ -44,57 +44,6 @@ function list(table) {
     })
 }
 
-<<<<<<< HEAD
-function get(table, id) {
-    return new Promise((resolve, reject) => {
-        connection.query(`SELECT * FROM ${table} WHERE id=${id}`, (err, data) => {
-            if (err) return reject(err);
-            resolve(data);
-        })
-    })
-}
-
-function insert(table, data) {
-    return new Promise((resolve, reject) => {
-        connection.query(`INSERT INTO ${table} SET ?`, data, (err, result) => {
-            if (err) return reject(err);
-            resolve(result);
-        })
-    })
-}
-
-function update(table, data) {
-    return new Promise((resolve, reject) => {
-        connection.query(`UPDATE ${table} SET ? WHERE id=?`, [data, data.id], (err, result) => {
-            if (err) return reject(err);
-            resolve(result);
-        })
-    })
-}
-
-function upsert(table, data) {
-    if (data && data.id) {
-        return update(table, data);
-    } else {
-        return insert(table, data);
-    }
-}
-
-function query(table, query) {
-    return new Promise((resolve, reject) => {
-        connection.query(`SELECT * FROM ${table} WHERE ?`, query, (err, res) => {
-            if (err) return reject(err);
-            resolve(res[0] || null);
-        })
-    })
-}
-
-module.exports = {
-    list,
-    get,
-    upsert,
-    query
-=======
 // function get(table, id) {
 //     return new Promise((resolve, reject) => {
 //         connection.query(`SELECT * FROM ${table} WHERE id=${id}`, (err, data) => {
@@ -144,5 +93,4 @@ module.exports = {
     // get,
     // upsert,
     // query
->>>>>>> prueba
 };
