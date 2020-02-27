47af75584bc9ff37b9f48470549d6b57a91cc1f0
esteban <emeza06@misena.edu.co>
2020-02-27 14:18:51 -0500

    ya el programa esta listo para jugar con la base de datos

--- api/index.js
+++ api/index.js
@@ -4,13 +4,16 @@ const config=require('../config.js');
 // const user =require(`../components/user/network`);
 const user =require('./components/user/network');
 const auth =require('./components/auth/network');
+const listasEmpaque =require('./components/listasEmpaque/network');
 const errors = require (`../network/errors`);
- const app= express();
+
+const app= express();
 app.use(bodyParser.json());
 //router
 
 app.use('/api/user',user);
 app.use('/api/auth',auth);
+app.use('/api/listasEmpaque',listasEmpaque);
 app.use(errors);
 app.listen( config.api.port,()=>{
     console.log('Api escuchando en el puerto',config.api.port);
