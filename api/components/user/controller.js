3ca93fd1bdbe819db8f8a33cb8ba8b704609e1d9
esteban <emeza06@misena.edu.co>
2020-03-17 10:31:06 -0500

    ya esta mas que listo

--- api/components/user/controller.js
+++ api/components/user/controller.js
@@ -1,58 +1,58 @@
-// const store =require('../../../sotore/gadolDbMysql');
-// const nanoId =require('nanoid');
-// const AUTH =require(`../auth`)
-// const TABLA =`user`;
+const store =require('../../../sotore/gadolDbMysql');
+const nanoId =require('nanoid');
+const AUTH =require(`../auth`)
+const TABLA =`user`;
 
 
-// module.exports= function (injectedStore) {
+module.exports= function (injectedStore) {
 
-//     let store=injectedStore;
+    let store=injectedStore;
 
-//     if(!store){
-//         store=require('../../../sotore/gadolDbMysql')
-//     }
+    if(!store){
+        store=require('../../../sotore/gadolDbMysql')
+    }
 
-//     function list() {
-//         return store.list(TABLA);
-//     }
+    function list() {
+        return store.list(TABLA);
+    }
 
-//     function get(id) {
-//         return store.get(TABLA,id);
-//     }
+    function get(id) {
+        return store.get(TABLA,id);
+    }
     
 
-//    async function upsert(body) {
-//         const user={
-//          id:body.id,
-//          name: body.name,
-//          username:body.username
-//         };
-//         // if(body.id){
-//         //     user.id=body.id
-//         // }
-//         // else{
-//         //     user.id=nanoId();
-//         // }
+   async function upsert(body) {
+        const user={
+         id:body.id,
+         name: body.name,
+         username:body.username
+        };
+        // if(body.id){
+        //     user.id=body.id
+        // }
+        // else{
+        //     user.id=nanoId();
+        // }
 
-//         if (body.password || body.username){
-//             await AUTH.upsert(
-//                 {
-//                     id:user.id,
-//                     username:user.username,
-//                     password:body.password,
-//                 }
-//             )
+        if (body.password || body.username){
+            await AUTH.upsert(
+                {
+                    id:user.id,
+                    username:user.username,
+                    password:body.password,
+                }
+            )
             
-//         }
+        }
         
 
-//         return store.upsert(TABLA,user);
-//     }
+        return store.upsert(TABLA,user);
+    }
 
 
-//     return{
-//         list,
-//         get,
-//         upsert
-//     };  
-// }
+    return{
+        list,
+        get,
+        upsert
+    };  
+}
--- api/components/user/network.js
+++ api/components/user/network.js
@@ -1,38 +1,38 @@
-// const express =require('express');
-// const secure=require('./secure');
-// const response=require(`../../../network/response`)
-// const Controller =require(`./index`);
+const express =require('express');
+const secure=require('./secure');
+const response=require(`../../../network/response`)
+const Controller =require(`./index`);
 
 
-// const router=express.Router();
-// //routes
-// router.get('/',list)
-// router.get('/:id',get)
-// router.post('/',upsert)
-// router.put('/', secure('update'),upsert)
+const router=express.Router();
+//routes
+router.get('/',list)
+router.get('/:id',get)
+router.post('/',upsert)
+router.put('/', secure('update'),upsert)
 
-// function list(req,res,next) {
-//     // res.send('todo esta en orden');
-//    Controller.list()
-//    .then((lista)=> {
-//     response.success(req,res,lista,200 )
-//    })
-//    .catch(next);
-// }
-// function get(req,res,next) {
-//     Controller.get(req.params.id)
-//         .then((user)=>{
-//             response.success(req,res,user,200)
-//         })
-//         .catch(next);
-//     }
+function list(req,res,next) {
+    // res.send('todo esta en orden');
+   Controller.list()
+   .then((lista)=> {
+    response.success(req,res,lista,200 )
+   })
+   .catch(next);
+}
+function get(req,res,next) {
+    Controller.get(req.params.id)
+        .then((user)=>{
+            response.success(req,res,user,200)
+        })
+        .catch(next);
+    }
    
-// function upsert(req,res,next) {
-//         Controller.upsert(req.body)
-//    .then((user)=>{
-//         response.success(req,res,user,200)
-//    })
-//    .catch(next);
-// }
+function upsert(req,res,next) {
+        Controller.upsert(req.body)
+   .then((user)=>{
+        response.success(req,res,user,200)
+   })
+   .catch(next);
+}
 
-// module.exports=router;
+module.exports=router;
