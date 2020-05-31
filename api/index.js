f94f68fa03cc21dfa9fc29cdb36d8644444d5cc6
esteban <emeza06@misena.edu.co>
2020-05-30 19:43:58 -0500

    make it better

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ const errors = require(`../network/errors`);
 
 const app = express();
 app.use((req, res, next) => {   
-    res.header('Access-Control-Allow-Origin', 'https://a-tu-servico.web.app/');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
 
     // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
